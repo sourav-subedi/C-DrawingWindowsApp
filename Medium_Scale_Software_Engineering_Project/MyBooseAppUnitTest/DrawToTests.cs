2d6399500e92e182720ce3e02fc67c9b34dd27de
Sourav Subedi <subedisourav8@gmail.com>
2025-12-04 14:48:23 +0545

    created test method for drawto and multiline command

---
+++ Medium_Scale_Software_Engineering_Project/MyBooseAppUnitTest/DrawToTests.cs
@@ -0,0 +1,40 @@
+﻿using BOOSE;
+using MYBooseApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooseAppUnitTest
+{
+    [TestClass]
+    public class DrawToTests
+    {
+        private AppCanvas canvas;
+        private StoredProgram program;
+        private AppCommandFactory factory;
+        private AppParser parser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            canvas = new AppCanvas(500, 500);
+            factory = new AppCommandFactory();
+            program = new StoredProgram(canvas);
+            parser = new AppParser(factory, program);
+        }
+
+        [TestMethod]
+        public void DrawTo_UpdatesPenPositionCorrectly()
+        {
+            string code = "drawto 300,400";
+
+            parser.Parse(code);
+            program.Run();
+
+            Assert.AreEqual(300, canvas.Xpos);
+            Assert.AreEqual(400, canvas.Ypos);
+        }
+    }
+}
---
+++ Medium_Scale_Software_Engineering_Project/MyBooseAppUnitTest/MultiLineTests.cs
@@ -0,0 +1,45 @@
+﻿using BOOSE;
+using MYBooseApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooseAppUnitTest
+{
+    [TestClass]
+    public class MultiLineTests
+    {
+        private AppCanvas canvas;
+        private StoredProgram program;
+        private AppCommandFactory factory;
+        private AppParser parser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            canvas = new AppCanvas(500, 500);
+            factory = new AppCommandFactory();
+            program = new StoredProgram(canvas);
+            parser = new AppParser(factory, program);
+        }
+
+        [TestMethod]
+        public void MultiLineProgram_ExecutesCorrectly()
+        {
+            string code = @"
+                moveto 50,50
+                circle 40
+                drawto 200,200
+                rect 100,50
+            ";
+
+            parser.Parse(code);
+            program.Run();
+
+            Assert.AreEqual(200, canvas.Xpos);
+            Assert.AreEqual(200, canvas.Ypos);
+        }
+    }
+}
