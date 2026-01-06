1be292ff0aa4240ca8d7e9e240c050425b2bc477
Sourav Subedi <subedisourav8@gmail.com>
2026-01-06 20:59:10 +0545

    fixed issue with custom array class

--- Medium_Scale_Software_Engineering_Project/Medium_Scale_Software_Engineering_Project/AppArray.cs
+++ Medium_Scale_Software_Engineering_Project/Medium_Scale_Software_Engineering_Project/AppArray.cs
@@ -1,40 +1,11 @@
 ﻿using BOOSE;
-using System.Reflection;
 
 namespace MYBooseApp
 {
     public class AppArray : BOOSE.Array
     {
-        public AppArray()
-        {
-            // Reset the static counter using reflection
-            ResetArrayCounter();
-        }
-
-        private void ResetArrayCounter()
-        {
-            try
-            {
-                var arrayType = typeof(BOOSE.Array);
-                var fields = arrayType.GetFields(BindingFlags.NonPublic | BindingFlags.Static);
-                foreach (var field in fields)
-                {
-                    if (field.FieldType == typeof(int))
-                    {
-                        field.SetValue(null, 0); // Reset counter to 0
-                        break;
-                    }
-                }
-            }
-            catch
-            {
-                // If reflection fails, ignore
-            }
-        }
-
         public override void CheckParameters(string[] parameterList)
         {
-            
             base.Parameters = base.ParameterList.Trim().Split(' ');
             if (base.Parameters.Length != 3 && base.Parameters.Length != 4)
             {
--- Medium_Scale_Software_Engineering_Project/Medium_Scale_Software_Engineering_Project/AppCommandFactory.cs
+++ Medium_Scale_Software_Engineering_Project/Medium_Scale_Software_Engineering_Project/AppCommandFactory.cs
@@ -1,13 +1,29 @@
 ﻿using BOOSE;
 using MYBooseApp;
-
-
+using System.Reflection;
 
 namespace MYBooseApp
 {
-
     public class AppCommandFactory : CommandFactory
     {
+        private void ResetArrayCounter()
+        {
+            try
+            {
+                var arrayType = typeof(BOOSE.Array);
+                var fields = arrayType.GetFields(BindingFlags.NonPublic | BindingFlags.Static);
+                foreach (var field in fields)
+                {
+                    if (field.FieldType == typeof(int))
+                    {
+                        field.SetValue(null, 0);
+                        break;
+                    }
+                }
+            }
+            catch { }
+        }
+
         public override ICommand MakeCommand(string commandType)
         {
             commandType = commandType.ToLower().Trim();
@@ -18,6 +34,27 @@ namespace MYBooseApp
             if (commandType == "real")
                 return new AppReal();
 
+            if (commandType == "array")
+            {
+                ResetArrayCounter();  // Reset before creating
+                return new AppArray();
+            }
+
+            if (commandType == "poke")
+            {
+                ResetArrayCounter();  // Reset before creating
+                return new AppPoke();
+            }
+
+            if (commandType == "peek")
+            {
+                ResetArrayCounter();  // Reset before creating
+                return new AppPeek();
+            }
+
+            if (commandType == "write")
+                return new AppWrite();
+
             if (commandType == "moveto")
                 return new AppMoveto();
 
@@ -27,21 +64,7 @@ namespace MYBooseApp
             if (commandType == "rect" || commandType == "rectangle")
                 return new AppRect();
 
-            if (commandType == "write")
-                return new AppWrite();
-
-            if (commandType == "array")
-                return new AppArray();
-
-            if (commandType == "poke")
-                return new AppPoke();
-
-            if (commandType == "peek")
-                return new AppPeek();
-
             return base.MakeCommand(commandType);
         }
-
-
     }
 }
